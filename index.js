1ccef6098faac3bcdc3a4b4e64e30723e8240a2b
Ajit Garg <ajit.garg08@gmail.com>
2024-10-24 23:27:05 +0530

    removed backend as not required

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-import express from "express";
-const app = express();    // object
-const port = 3000;
-
-app.use(express.static('public'));
-
-
-// app.listen(port, callback);    //listen method
-app.listen(port, () => {
-    console.log(`Server running on port ${port}.`);
-});
+// document.getElementById("locate-iss-btn")
