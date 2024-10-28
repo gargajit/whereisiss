5b59f29cb8fdd2f842ecb84c375e6badf3da12d0
Ajit Garg <ajit.garg08@gmail.com>
2024-10-28 23:00:26 +0530

    working on js part

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-// document.getElementById("locate-iss-btn")
+$("#locate-iss-btn").click(function() {
+    // $("#iss-location").innerHTML(`<p class="coordinates">Latitude: </p><p class="coordinates">Longitude: </p>`);
+});
