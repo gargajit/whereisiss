d6d120dd478ab6dd4af2c8ca2007a77967e826a1
Ajit Garg <ajit.garg08@gmail.com>
2024-11-04 23:45:50 +0530

    making improvements

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 $("#locate-iss-btn").click(function() {
-    // $("#iss-location").innerHTML(`<p class="coordinates">Latitude: </p><p class="coordinates">Longitude: </p>`);
+    $("#iss-location").slideToggle("slow");
 });
