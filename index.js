5166c8b0e83d3c5b351be79d200c5b74c9386a00
Ajit Garg <ajit.garg08@gmail.com>
2024-11-06 23:17:32 +0530

    adding information to the iss-location div

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 $("#locate-iss-btn").click(function() {
-    $(".titan-one-regular").text("Currently ISS is at:")
+    $(".titan-one-regular").text("Currently ISS is at:");
     $("#iss-location").slideToggle("slow");
+    $("#iss-location").html("<p><b>Lattitude: </b></p> <br /><p><b>Longitude: </b></p>");
 });
