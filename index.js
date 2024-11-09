aecb05b80e08620d886cbfa55dad9695d1c3da80
Ajit Garg <ajit.garg08@gmail.com>
2024-11-09 23:31:55 +0530

    no changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $("#locate-iss-btn").click(function() {
     $(".titan-one-regular").text("Currently ISS is at:");
     $("#iss-location").slideToggle("slow");
-    $("#iss-location").html("<p><b>Lattitude: </b></p> <br /><p><b>Longitude: </b></p>");
+    $("#iss-location").html("<p><b>Lattitude: </b></p><br /><p><b>Longitude: </b></p>");
 });
